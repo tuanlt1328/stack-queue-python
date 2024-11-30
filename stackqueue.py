4cd07b08a8e80eea92c10ef585364c11eaf99806
tuanlt1328 <111944260+tuanlt1328@users.noreply.github.com>
2024-11-30 08:32:12 +0700

    Add files via upload

---
+++ stackqueue.py
@@ -0,0 +1,40 @@
+class stack:
+    def __init__(self):
+        self.data=[]
+    def isEmpty(self) -> bool:
+        return len(self.data)==0
+    def size(self):
+        return len(self.data)
+    def push(self, val):
+        self.data.append(val)
+    def pop(self):
+        if not self.isEmpty:
+            s=self.data[len(self.data)-1]
+            del self.data[len(self.data)-1]
+            return s
+        raise ValueError("There are nothing to pop")
+    def first(self):
+        return self.data[0]
+    def last(self):
+        return self.data[len(self.data)-1]
+
+class queue:
+    def __init__(self):
+        self.data=[]
+    def isEmpty(self) -> bool:
+        return len(self.data)==0
+    def size(self):
+        return len(self.data)
+    def enqueue(self, val):
+        self.data.append(val)
+    def dequeue(self):
+        if not self.isEmpty:
+            s=self.data[0]
+            del self.data[0]
+            return s
+        raise ValueError("There are nothing to dequeue")
+    def first(self):
+        return self.data[0]
+    def last(self):
+        return self.data[len(self.data)-1]
+
